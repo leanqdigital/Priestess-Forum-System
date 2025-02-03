e0f561c1bba109ffaa82f86369438eba0cfda99e
Uttam Pradhan <uttampra1@gmail.com>
2025-02-03 13:25:42 +0545

    Refactor comment structure and styling for improved layout and accessibility; enhance delete button interactions and responsiveness.

--- script/app.js
+++ script/app.js
@@ -359,8 +359,8 @@ class ForumManager {
   getVoteSVG(isVoted) {
     return `
       <svg width="24" height="24" viewBox="0 0 24 24" 
-          fill="${isVoted ? "#044047" : "none"}" 
-          stroke="#044047">
+          fill="${isVoted ? "var(--secondary)" : "none"}" 
+          stroke="var(--secondary)">
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
       </svg>
   `;
@@ -837,7 +837,7 @@ class PostModalManager {
       <h3 class="text-lg font-semibold text-white mb-4">Comments</h3>
       
       <!-- Add comment form -->
-      <div class="comment-form bg-primary-100 rounded-lg p-4 mb-6">
+      <div class="comment-form bg-primary-100 rounded-lg min-[500px]:p-4 mb-6">
         <div id="comment-editor" 
              class="comment-editor text-white p-2 min-h-[100px] border border-gray-600 rounded mb-3" 
              contenteditable="true"
