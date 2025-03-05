6d62a1889adaac89da5593a59dca43b0db24e24a
Dipesh <dpes.adikari44@gmail.com>
2025-03-05 09:38:27 +0545

    use display name and forum image in tagging

--- script/class/classContactService.js
+++ script/class/classContactService.js
@@ -16,6 +16,8 @@ class ContactService {
             Registered_Member_First_Name: field(arg: ["Registered_Member", "first_name"])
             Registered_Member_Last_Name: field(arg: ["Registered_Member", "last_name"])
             Registered_Member_Profile_Image: field(arg: ["Registered_Member", "profile_image"])
+            Registered_Member_Display_Name: field(arg: ["Registered_Member", "display_name"])
+            Registered_Member_Forum_Image: field(arg: ["Registered_Member", "forum_image"])
           }
         }
       `;
@@ -26,9 +28,9 @@ class ContactService {
 
       return data.calcRegisteredMembersRegisteredCoursesMany.map((contact) => ({
         id: contact.Registered_Member_Contact_ID,
-        name: `${contact.Registered_Member_First_Name} ${contact.Registered_Member_Last_Name}`,
+        name: contact.Registered_Member_Display_Name || "Anonymous",
         profileImage:
-          contact.Registered_Member_Profile_Image ||
+          contact.Registered_Member_Forum_Image ||
           CONFIG.api.defaultAuthorImage,
       }));
     } catch (error) {
