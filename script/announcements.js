badc0252a5628920e64ed1b982c728a01487dc30
Dipesh Adhikari <dpes@adikari44@gmail.com>
2025-04-02 16:07:29 +0545

    na

--- script/announcements.js
+++ script/announcements.js
@@ -53,7 +53,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const SUBSCRIPTION_QUERY = `
   subscription subscribeToCalcAnnouncements(
-    $created_at: TimestampSecondsScalar,
     $related_course_id: PriestessCourseID,
     $author_id: PriestessContactID,
     $id: PriestessContactID,
@@ -62,8 +61,6 @@ document.addEventListener('DOMContentLoaded', function () {
   ) {
     subscribeToCalcAnnouncements(
       query: [
-        { where: { created_at: $created_at, _OPERATOR_: gt } },
-        ${fetchUserDate}
         {
           andWhereGroup: [
             {
