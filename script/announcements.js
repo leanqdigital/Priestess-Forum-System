5547ec1f6d4522922689536ac15cab211cdc2e5a
dpes44 <dpes.adikari44@gmail.com>
2025-04-24 13:32:21 +0545

    removed settings

--- script/announcements.js
+++ script/announcements.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
           andWhereGroup: [
             {
               whereGroup: [
-                { where: { announcement__type: "${POSTS_TYPE}" } },
+                { where: { announcement__type: "Post" } },
                 { andWhere: { Post: [ { where: { related_course_id: $related_course_id } } ] } },
                 { andWhere: { Post: [ { where: { author_id: $author_id, _OPERATOR_: neq } } ] } },
                 { andWhere: { Post: [ { where: { post_type: "Notification" } } ] } }
@@ -76,14 +76,14 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             {
               orWhereGroup: [
-                { where: { announcement__type: "${POST_COMMENTS_TYPE}"} },
+                { where: { announcement__type: "Comment"} },
                 { andWhere: { Comment: [ { where: { Forum_Post: [ { where: { related_course_id: $related_course_id } }, { andWhere: { author_id: $author_id } } ] } } ] } },
                 { andWhere: { Comment: [ { where: { author_id: $author_id, _OPERATOR_: neq } } ] } }
               ]
             },
             {
             orWhereGroup: [
-              { where: { announcement__type: "${MENTION.comment}"} },
+              { where: { announcement__type: "Comment Mention"} },
               { andWhere: { Comment: [ { where: { author_id: $author_id, _OPERATOR_: neq } } ] } },
               { andWhere: { Comment: [ { where: { Comment_or_Reply_Mentions: [{ where: { id: $id } } ] } } ] } },
               { andWhere: { Comment: [ { where: { Forum_Post: [ { where: { related_course_id: $related_course_id } } ] } } ]} }
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
           },
           {
               orWhereGroup: [
-                { where: { announcement__type: "${MENTION.post}" } },
+                { where: { announcement__type: "Post Mention" } },
                 { andWhere: { Post: [ { where: { related_course_id: $related_course_id } } ] } },
                 { andWhere: { Post: [ { where: { Mentioned_Users: [ { where: { id: $id } } ] } }, { andWhere: { author_id: $author_id, _OPERATOR_: neq } } ] } }
               ]
