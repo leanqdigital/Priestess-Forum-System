f2f49aab21170c588ca072ecf5ba2c9706e574e4
dpes44 <dpes.adikari44@gmail.com>
2025-05-14 10:06:05 +0545

    fetch contacts query fix

--- script/class/classContactService.js
+++ script/class/classContactService.js
@@ -2,52 +2,66 @@ class ContactService {
   static async fetchContacts(courseID) {
     try {
       const query = `
-        query calcRegisteredMembersRegisteredCoursesMany(
-          $registered_course_id: PriestessCourseID
-          $name: TextScalar
-          $limit: IntScalar
-          $offset: IntScalar
-        ) {
-          calcRegisteredMembersRegisteredCoursesMany(
-            query: [
-              {
-                where: {
-                  registered_course_id: $registered_course_id
-                }
-              }
-              {
-                andWhere: {
-                  Registered_Member: [
-                    {
-                      where: {
-                        TagsData: [
-                          {
-                            where: {
-                              Tag: [
-                                {
-                                  where: {
-                                    name: $name
-                                  }
-                                }
-                              ]
-                            }
-                          }
-                        ]
-                      }
+            query calcRegisteredMembersRegisteredCoursesMany(
+              $registered_course_id: PriestessCourseID
+              $name: TextScalar
+              $limit: IntScalar
+              $offset: IntScalar
+            ) {
+              calcRegisteredMembersRegisteredCoursesMany(
+                query: [
+                  {
+                    where: {
+                      registered_course_id: $registered_course_id
                     }
-                  ]
-                }
+                  }
+                  {
+                    andWhere: {
+                      Registered_Member: [
+                        {
+                          where: {
+                            TagsData: [
+                              {
+                                where: {
+                                  Tag: [{ where: { name: $name } }]
+                                }
+                              }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                  }
+                ]
+                limit: $limit
+                offset: $offset
+                orderBy: [
+                  {
+                    path: ["Registered_Member", "display_name"]
+                    type: asc
+                  }
+                ]
+              ) {
+                Registered_Member_Contact_ID: field(
+                  arg: ["Registered_Member", "id"]
+                )
+                Registered_Member_First_Name: field(
+                  arg: ["Registered_Member", "first_name"]
+                )
+                Registered_Member_Last_Name: field(
+                  arg: ["Registered_Member", "last_name"]
+                )
+                Registered_Member_Profile_Image: field(
+                  arg: ["Registered_Member", "profile_image"]
+                )
+                Registered_Member_Display_Name: field(
+                  arg: ["Registered_Member", "display_name"]
+                )
+                Registered_Member_Forum_Image: field(
+                  arg: ["Registered_Member", "forum_image"]
+                )
               }
-            ]
-          ) {
-            Registered_Member_Contact_ID: field(arg: ["Registered_Member", "id"])
-            Registered_Member_First_Name: field(arg: ["Registered_Member", "first_name"])
-            Registered_Member_Last_Name: field(arg: ["Registered_Member", "last_name"])
-            Registered_Member_Profile_Image: field(arg: ["Registered_Member", "profile_image"])
-            Registered_Member_Display_Name: field(arg: ["Registered_Member", "display_name"])
-            Registered_Member_Forum_Image: field(arg: ["Registered_Member", "forum_image"])
-          }
-        }
+            }
       `;
 
       const variables = { 
